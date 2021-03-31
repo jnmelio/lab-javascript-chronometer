86ccf88950068880838ab63dc2e709d64ded56b9
Julie <julienonnenmacher@hotmail.fr>
2021-03-31 13:03:41 +0200

    omg

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,21 +1,36 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = 0
   }
   startClick(callback) {
     // ... your code goes here
+    this.intervalId = setInterval (() => {
+      this.currentTime++
+      // callback()
+    }, 1000)
   }
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60)
+
   }
   getSeconds() {
     // ... your code goes here
+    return this.currentTime
   }
-  twoDigitsNumber() {
+  twoDigitsNumber(num) {
     // ... your code goes here
+    if (num <= 9) {
+      return "0" + num.toString()
+    } else {
+      return num.toString()
+    }
   }
   stopClick() {
     // ... your code goes here
+    clearInterval(this.intervalId)
   }
   resetClick() {
     // ... your code goes here
